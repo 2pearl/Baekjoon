1349a2abfbab99d638d66c97a07eb29b468dfa50
2pearl <74369702+2pearl@users.noreply.github.com>
2024-02-02 13:36:31 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 264 ms, Memory: 16988 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -3,39 +3,37 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st;
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		st = new StringTokenizer(r.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-
-		Queue<Integer> queue = new LinkedList<>();
-		for (int i = 1; i < N + 1; i++)
+		
+		Queue<Integer>queue=new ArrayDeque<>();
+		
+		for(int i=1;i<=N;i++)
 			queue.offer(i);
-
-		StringBuilder sb = new StringBuilder("<");
-		while (queue.size() > 1) {
-
-			for (int i = 0; i < K - 1; i++)
+		StringBuilder sb=new StringBuilder();
+		sb.append("<");
+		while(queue.size()>1) {
+			
+			for(int i=0;i<K-1;i++)
 				queue.offer(queue.poll());
-
 			sb.append(queue.poll()).append(", ");
 		}
 		sb.append(queue.poll()).append(">");
-
-		w.write(sb.toString());
-		w.flush();
-		w.close();
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
