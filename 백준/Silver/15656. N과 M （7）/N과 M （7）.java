c76d1a3fc1c311978aba6378136130d727b7fa7b
2pearl <74369702+2pearl@users.noreply.github.com>
2024-02-08 11:59:29 +0900

    [Silver III] Title: N과 M (7), Time: 660 ms, Memory: 123148 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N,M;
+	static int[]input, res;
+	static StringBuilder sb;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+	
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		sb=new StringBuilder();
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		st=new StringTokenizer(br.readLine());
+		input=new int[N];
+		for(int i=0;i<N;i++) {
+			input[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		
+		res=new int[M];
+		dfs(0,0);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+	
+	public static void dfs(int depth,int idx) {
+			
+		if(depth==M) {
+			for(int i=0;i<M;i++) {
+				sb.append(res[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0;i<N;i++) {
+			
+			res[depth]=input[i];
+			dfs(depth+1,i);
+		}
+	}
+}
