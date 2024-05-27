9cef78aaeca635b2f7ee716b885665e539c1a33d
2pearl <74369702+2pearl@users.noreply.github.com>
2024-05-27 15:35:36 +0900

    [Silver III] Title: N과 M (3), Time: 468 ms, Memory: 68476 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N,M;
+	static int[] result;
+	
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		result = new int[M];
+		
+		sb = new StringBuilder();
+
+		dfs(0);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	public static void dfs(int idx) {
+
+		if (idx == M) {
+			for (int data : result) {
+				sb.append(data).append(" ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
+
+		for(int i=1;i<=N;i++) {
+			result[idx]=i;
+			dfs(idx+1);
+		}
+	}
+}
