92120ece5e0df5eea1ab306d01e0655a3c83c080
2pearl <74369702+2pearl@users.noreply.github.com>
2024-05-29 14:09:33 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -3,58 +3,47 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int N=Integer.parseInt(br.readLine());
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		StringBuilder sb=new StringBuilder();
-		List<Taste> list=new ArrayList<>();
-		for(int i=0;i<N;i++) {
-			st=new StringTokenizer(br.readLine());
-			int shin=Integer.parseInt(st.nextToken());
-			int sseun=Integer.parseInt(st.nextToken());
-			
-			list.add(new Taste(shin,sseun));
+
+		int[][] arr = new int[N][2];
+		for (int i = 0; i < N; i++) {
+
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());// 신맛
+			arr[i][1] = Integer.parseInt(st.nextToken());// 쓴맛
 		}
-		
-		int min=Integer.MAX_VALUE;
-		for(int i=1;i<(1<<N);i++) {//1~15
-			
-			int sSum=1;
-			int ssSum=0;
-			for(int j=0;j<N;j++) {
-				
-				if(((i&(1<<j))!=0)) {
-					
-					sSum*=list.get(j).shin;
-					ssSum+=list.get(j).sseun;
+
+		int min = Integer.MAX_VALUE;
+		for (int i = 1; i < (1 << N); i++) {// 모든 부분집합
+
+			int shin = 1;
+			int sseun = 0;
+
+			for (int j = 0; j < N; j++) {
+
+				if ((i & (1 << j)) > 0) {
+
+					shin *= arr[j][0];
+					sseun += arr[j][1];
 				}
-					
 			}
-			min=Math.min(min, Math.abs(sSum-ssSum));
-			
+
+			min = Math.min(min, Math.abs(shin - sseun));
 		}
+
 		bw.write(String.valueOf(min));
 		bw.flush();
 		bw.close();
 		br.close();
-	}
-	
-}
-class Taste{
-	int shin;
-	int sseun;
-	Taste(int shin,int sseun){
-		this.shin=shin;
-		this.sseun=sseun;
 	}
 }
