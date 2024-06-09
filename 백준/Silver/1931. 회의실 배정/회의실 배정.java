f31f15607683a7350d56416414c64cddb117e2b5
2pearl <74369702+2pearl@users.noreply.github.com>
2024-06-09 10:15:28 +0900

    [Silver I] Title: 회의실 배정, Time: 520 ms, Memory: 43816 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,59 +1,50 @@
-//백준
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
 
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[][] meetings;
 
-		int N=Integer.parseInt(r.readLine());
-		int[][]a=new int[N][2];
-		StringTokenizer st;
-		for(int i=0;i<N;i++) {
-			st=new StringTokenizer(r.readLine());
-			a[i][0]=Integer.parseInt(st.nextToken());
-			a[i][1]=Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(a,new Comparator<int[]>() {
+    public static void main(String[] args) throws IOException {
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[1]>o2[1])
-					return 1;
-				else if(o1[1]<o2[1])
-					return -1;
-				else {
-					if(o1[0]>o2[0])
-						return 1;
-					else if(o1[0]<o2[0])
-						return -1;
-					else return 0;
-				}
-			}
-		});
-		int count=0;
-		int prev=0;
-		
-		for(int i=0;i<N;i++) {
-			
-			if(prev<=a[i][0]) {
-				prev=a[i][1];
-				count++;
-			}
-		}
-		
-		w.write(String.valueOf(count));
-		w.flush();
-		w.close();
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        meetings = new int[N][2];
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            meetings[i][0] = start;
+            meetings[i][1] = end;
+        }//입력끝
+
+        Arrays.sort(meetings, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                //끝나는 시간 오름차순
+                return o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1];
+            }
+        });
+
+        int ans = 0;
+        int h = 0;
+        for (int i = 0; i < N; i++) {
+
+            if (h <= meetings[i][0]) {
+                h = meetings[i][1];
+                ans++;
+            }
+        }
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
