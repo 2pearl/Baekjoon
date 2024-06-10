0da2bae440b66f4a51c1189404aecdf06baba7fc
2pearl <74369702+2pearl@users.noreply.github.com>
2024-06-10 11:04:38 +0900

    [Silver II] Title: 색종이 만들기, Time: 204 ms, Memory: 17004 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,72 +1,64 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int[][] arr;
+    static int white, blue;
 
-	static int N;
-	static int[][] input;
-	static int white,blue;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+        N = Integer.parseInt(br.readLine());
+        arr=new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }//입력끝
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        dfs(0, 0, N);
+        bw.write(white + "\n" + blue);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-		N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+    //d는 칸
+    public static void dfs(int r, int c, int d) {
 
-		input = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+        if (check(r, c, d)) {
+            //흰색은 0,파란색은 1
+            if (arr[r][c] == 0) {
+                white++;
+            } else {
+                blue++;
+            }
+            return;
+        }
 
-			for (int j = 0; j < N; j++) {
-				input[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		//0이면 흰,1이면 파랑
-		
-		divide(0,0,N);//8
-		bw.write(String.valueOf(white)+"\n"+String.valueOf(blue));
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        //아니면 더 나눠야함
+        int next = d / 2;
+        dfs(r, c, next);
+        dfs(r, c + next, next);
+        dfs(r + next, c, next);
+        dfs(r + next, c + next, next);
+    }
 
-	public static void divide(int row,int col,int d) {
+    //다 같은 색인지 확인하는 메서드
+    public static boolean check(int r, int c, int d) {
 
-		if(check(row,col,d)) {
-			
-			if(input[row][col]==0)
-				white++;
-			else blue++;
-			
-			return;
-		}
-		
-		int next=d/2;
-		divide(row,col,next);
-		divide(row,col+next,next);
-		divide(row+next,col,next);
-		divide(row+next,col+next,next);
-
-	}
-	public static boolean check(int row,int col,int d) {
-		
-		int color=input[row][col];
-		
-		for (int i = row; i < row+d; i++) {
-			for (int j = col; j < col+d; j++) {
-				
-				//다른 색상
-				if(input[i][j]!=color) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+        int color = arr[r][c];
+        for (int i = r; i < r + d; i++) {
+            for (int j = c; j < c + d; j++) {
+                if (arr[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
