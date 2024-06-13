19b38ff70b12d5bdcccc112167eb828b48565d49
2pearl <74369702+2pearl@users.noreply.github.com>
2024-06-13 11:32:12 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 16744 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,99 +1,83 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-class Point {
-	int row;
-	int col;
-	char color;
-
-	public Point(int row, int col, char color) {
-		super();
-		this.row = row;
-		this.col = col;
-		this.color = color;
-	}
-}
-
 public class Main {
 
-	static char[][] arr;
-	static boolean[][] visited;
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-	static int N;
+    static int N;
+    static char[][] map;
+    static boolean[][] visited;
+    static int ans1, ans2;//적록색약 아닌사람, 맞는사람(r,g구분 못함)
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j);
+            }
+        }//입력끝
 
-		N = Integer.parseInt(br.readLine());
-		arr = new char[N][N];
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < N; j++)
-				arr[i][j] = str.charAt(j);
-		}
+        //적록색약아닌사람
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    ans1++;
+                    bfs(i, j);
+                }
+            }
+        }
 
-		// 비 적록색약
-		int not = 0;
-		visited = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j]) {
-					not++;
-					bfs(i, j);
-				}
-			}
-		}
+        //g를 r로 변경
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 'G')
+                    map[i][j] = 'R';
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (arr[i][j] == 'G')
-					arr[i][j] = 'R';// 초록색을 빨강으로 변경하깅
-			}
-		}
 
-		// 적록색약
-		int yes = 0;
-		visited = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j]) {
-					yes++;
-					bfs(i, j);
-				}
-			}
-		}
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    ans2++;
+                    bfs(i, j);
+                }
+            }
+        }
 
-		bw.write(String.valueOf(not) + " " + String.valueOf(yes));
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        bw.write(ans1 + " " + ans2);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-	public static void bfs(int i, int j) {// 비 적록색약
+    public static void bfs(int i, int j) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{i, j});
+        visited[i][j] = true;
 
-		Queue<Point> queue = new ArrayDeque<>();
-		queue.offer(new Point(i, j, arr[i][j]));
-		visited[i][j] = true;
-		while (!queue.isEmpty()) {
-			Point now = queue.poll();
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int nr = now[0] + dr[d];
+                int nc = now[1] + dc[d];
 
-			for (int d = 0; d < 4; d++) {
-				int nextr = now.row + dr[d];
-				int nextc = now.col + dc[d];
-
-				if (nextr > -1 && nextr < N && nextc > -1 && nextc < N && arr[nextr][nextc] == now.color&&!visited[nextr][nextc]) {
-					visited[nextr][nextc]=true;
-					queue.offer(new Point(nextr,nextc,arr[nextr][nextc]));
-				}
-			}
-		}
-	}
+                if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                //같은 색상
+                if (map[nr][nc] == map[now[0]][now[1]]&&!visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+    }
 }
