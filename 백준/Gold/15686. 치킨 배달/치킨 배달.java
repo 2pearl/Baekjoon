4b36821dac21975c29bd5681cb023eddb42eb8e9
2pearl <74369702+2pearl@users.noreply.github.com>
2024-06-13 11:14:06 +0900

    [Gold V] Title: 치킨 배달, Time: 212 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,102 +1,68 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Point {
-
-	int row;
-	int col;
-
-	public Point(int row, int col) {
-		super();
-		this.row = row;
-		this.col = col;
-	}
-}
-
 public class Main {
+    static int N, M;
+    static List<int[]> homes;
+    static List<int[]> chickens;
+    static int[] pick;
+    static int min_dist;
 
-	static int N, M;
-	static int[] res;
-	static boolean[] visited;
-	static List<Point> home, chicken;
-	static int min;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        homes = new ArrayList<>();
+        chickens = new ArrayList<>();
+        pick = new int[M];
+        min_dist = Integer.MAX_VALUE;
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                if (n == 1) homes.add(new int[]{i, j});
+                else if (n == 2) chickens.add(new int[]{i, j});
+            }
+        }//입력끝
 
-		home = new ArrayList<>();
-		chicken = new ArrayList<>();
-		res = new int[M];
+        dfs(0, 0);
+        bw.write(String.valueOf(min_dist));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-		min = Integer.MAX_VALUE;
+    //치킨집 고르는 경우의 수
+    public static void dfs(int idx, int depth) {
 
-		// 입력
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				int n = Integer.parseInt(st.nextToken());
-				if (n == 1) {// 집
-					home.add(new Point(i, j));
-				} else if (n == 2) {// 치킨집
-					chicken.add(new Point(i, j));
-				}
-			}
-		}
+        if (depth == M) {
+            min_dist = Math.min(min_dist, getDistance());
+            return;
+        }
 
-		visited = new boolean[chicken.size()];
+        for (int i = idx; i < chickens.size(); i++) {
+            pick[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
 
-		// 조합 구하기
-		dfs(0,0);
-		
-		bw.write(String.valueOf(min));
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    public static int getDistance() {
+        int sum = 0;
+        for (int i = 0; i < homes.size(); i++) {
+            int m = Integer.MAX_VALUE;
+            for (int j = 0; j < M; j++) {
 
-	public static void dfs(int depth,int idx) {
-
-		if (depth == M) {// 치킨 집 다 구함
-
-			min = Math.min(min, getDistance());
-			return;
-		}
-
-		for (int i = idx; i < chicken.size(); i++) {
-			
-			res[depth]=i;
-			dfs(depth+1,i+1);
-		}
-	}
-
-	public static int getDistance() {
-		
-		int sum=0;
-		
-		for(int i=0;i<home.size();i++) {
-			
-			int m=Integer.MAX_VALUE;
-			
-			for(int j=0;j<M;j++) {
-				int dist=Math.abs(home.get(i).row-chicken.get(res[j]).row)+Math.abs(home.get(i).col-chicken.get(res[j]).col);
-				m=Math.min(m, dist);
-			}
-			sum+=m;
-		}
-		
-		return sum;
-	}
+                m = Math.min(m, Math.abs(homes.get(i)[0] - chickens.get(pick[j])[0]) + Math.abs(homes.get(i)[1] - chickens.get(pick[j])[1]));
+            }
+            sum += m;
+        }
+        return sum;
+    }
 }
