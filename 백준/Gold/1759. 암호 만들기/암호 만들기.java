174fc0c1b7bf4ad02765e1cc54803f63ccd75677
2pearl <74369702+2pearl@users.noreply.github.com>
2024-06-18 14:56:37 +0900

    [Gold V] Title: 암호 만들기, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,81 +1,66 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int L, C;
+    static char[] input;
+    static char[] picked;
+    static StringBuilder sb;
 
-	static int L, C;
-	static char[] alpha, res;
-	static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		
-		L = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        input = new char[C];
+        picked = new char[L];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            input[i] = st.nextToken().charAt(0);
+        }
 
-		alpha = new char[C];
-		res = new char[L];
+        Arrays.sort(input);
+        dfs(0, 0);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        bw.close();
 
-		// 저장
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < C; i++) {
-			alpha[i] = st.nextToken().charAt(0);
-		}
-		Arrays.sort(alpha);//알파벳 오름차순 정렬
-		
+    }
 
-		dfs(0, 0);
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    public static void dfs(int idx, int depth) {
+        if (depth == L) {
+            if (check()) {
+                for (char ch : picked) {
+                    sb.append(ch);
+                }
+                sb.append("\n");
+            }
+            return;
+        }
 
-	public static void dfs(int idx, int depth) {
+        for (int i = idx; i < C; i++) {
+            picked[depth] = input[i];
+            dfs(i + 1, depth + 1);
+        }
+    }
 
-		if (depth == L) {
-			if(check()) {
-				for(int i=0;i<L;i++)
-					sb.append(res[i]);
-				sb.append("\n");
-			}
-				
-			return;
-		}
+    public static boolean check() {
+        int m = 0;//모음 1개 이상
+        int z = 0;//자음 2개 이상
 
-		for (int i = idx; i < C; i++) {
-			res[depth] = alpha[i];
-
-			dfs(i + 1, depth + 1);
-
-		}
-	}
-
-	public static boolean check() {
-
-		int mo = 0;
-		int za = 0;
-
-		for (int i = 0; i < L; i++) {
-			if (res[i] == 'a' || res[i] == 'e' || res[i] == 'i' || res[i] == 'o' || res[i] == 'u')
-				mo++;
-			else
-				za++;
-		}
-
-		if (mo >= 1 && za >= 2)
-			return true;
-		else
-			return false;
-	}
+        for (char ch : picked) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                m++;
+            } else z++;
+        }
+        if (m >= 1 && z >= 2)
+            return true;
+        return false;
+    }
 }
