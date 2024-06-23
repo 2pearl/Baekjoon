debda5d58f15d2b028fd61fc0952392ca18183b0
2pearl <74369702+2pearl@users.noreply.github.com>
2024-06-23 15:01:38 +0900

    [Gold III] Title: 게리맨더링, Time: 152 ms, Memory: 15060 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,132 +1,107 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N;
+    static ArrayList<Integer>[] graph;
+    static int[] population;
+    static int min;
+    static boolean[] selected, visited;
 
-	static ArrayList<Integer>[] list;
-	static int[] peoples;
-	static int[] group;
-	static boolean[]visited;
-	static int min;
-	static int N;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-		N = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
+        population = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            population[i] = Integer.parseInt(st.nextToken());
+        }
 
-		list = new ArrayList[N + 1];
-		peoples = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) {
+                graph[i].add(Integer.parseInt(st.nextToken()));
+            }
+        }//입력끝
 
-		// list 생성
-		for (int i = 1; i <= N; i++) {
-			list[i] = new ArrayList<>();
-		}
+        min = Integer.MAX_VALUE;
+        selected = new boolean[N + 1];
 
-		// 인구
-		for (int i = 1; i <= N; i++) {
-			peoples[i] = Integer.parseInt(st.nextToken());
-		}
+        dfs(1);
+        bw.write(min == Integer.MAX_VALUE ? String.valueOf(-1) : String.valueOf(min));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-		// 인접구역
-		for (int i = 1; i <= N; i++) {
+    public static void dfs(int depth) {
 
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < n; j++)
-				list[i].add(Integer.parseInt(st.nextToken()));
-		}
-		// 입력끝
+        if (depth == N) {
+            List<Integer> a = new ArrayList<>();
+            List<Integer> b = new ArrayList<>();
 
-		int n = (int) Math.pow(2, N);// 경우의 수 2^N조합
-		int min = Integer.MAX_VALUE;
-		for (int i = 1; i < n; i++) {
+            //인구수
+            int acnt = 0;
+            int bcnt = 0;
 
-			group = new int[N + 1];
-			for (int k = 0; k < N; k++) {
-				if ((i & (1 << k)) > 0) {
-					group[k + 1] = 1;
-				}
-			}
-			// 그룹 나눔 0,1로 나눴고 index1부터 시작임
+            for (int i = 1; i < N + 1; i++) {
+                if (selected[i]) {
+                    a.add(i);
+                    acnt += population[i];
+                } else {
+                    b.add(i);
+                    bcnt += population[i];
+                }
+            }
 
-			min = Math.min(min, check());
-		}
-		
-		if(min==Integer.MAX_VALUE)
-			min=-1;
-		
-		bw.write(String.valueOf(min));
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+            if (a.size() == 0 || b.size() == 0)//한지역으로 몰빵이니까
+                return;
 
-	public static int check() {// 선거구 가능한지 확인
+            if (check(a) && check(b)) {
+                min = Math.min(min, Math.abs(acnt - bcnt));
+            }
 
-		// 불가능하면 Integer.max 보내장
-		ArrayList<Integer> red = new ArrayList<>();// 0
-		ArrayList<Integer> blue = new ArrayList<>();// 1
+            return;
+        }
 
-		int rcnt = 0;
-		int bcnt = 0;// 인구 수
-		// 그룹 나누기
-		for (int i = 1; i <= N; i++) {
-			if (group[i] == 0) {
+        selected[depth] = true;
+        dfs(depth + 1);
+        selected[depth] = false;
+        dfs(depth + 1);
+    }
 
-				red.add(i);
-				rcnt += peoples[i];
-			} else {
+    public static boolean check(List<Integer> list) {
 
-				blue.add(i);
-				bcnt += peoples[i];
-			}
-		}
-		if (red.size() == 0 || blue.size() == 0 || !isConnected(red) || !isConnected(blue))
-			return Integer.MAX_VALUE;
-		
-		
-		return Math.abs(rcnt - bcnt);
+        Queue<Integer> queue = new ArrayDeque<>();
+        visited = new boolean[N + 1];
+        queue.offer(list.get(0));
+        visited[list.get(0)] = true;
 
-	}
 
-	public static boolean isConnected(ArrayList<Integer> g) {
+        int cnt = 1;
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            for (int v : graph[now]) {
+                if (list.contains(v) && !visited[v]) {
+                    visited[v] = true;
+                    queue.offer(v);
+                    cnt++;
+                }
+            }
+        }
 
-		//이어져 있는지 확인
-		//bfs돌리자
-		
-		Queue<Integer>queue=new ArrayDeque<>();
-		visited=new boolean[N+1];
-		
-		queue.add(g.get(0));
-		visited[g.get(0)]=true;
-		
-		int cnt=0;
-		while(!queue.isEmpty()) {
-			int v=queue.poll();
-			
-			cnt++;
-			for(int to:list[v]) {
-				if(g.contains(to)&&!visited[to]) {
-					queue.offer(to);
-					visited[to]=true;
-				}
-			}
-		}
-		if(cnt==g.size())
-			return true;
-		return false;
-	}
+        if (cnt == list.size())
+            return true;
+        return false;
+    }
 }
