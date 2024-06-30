e6d641967696a12a773ab70aba789fbe2529ce74
2pearl <74369702+2pearl@users.noreply.github.com>
2024-06-30 21:06:22 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 236 ms, Memory: 20672 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,107 +1,94 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int[][] map;
-	static int[][] rupee;
-	static PriorityQueue<Point> pq;
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
+    static int N;
+    static int[][] map;
+    static int[][] rupee;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+    static final int INF = 1_000_000_000;
 
-	static class Point implements Comparable<Point> {
-		int row;
-		int col;
-		int weight;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int tc = 1;
+        while (true) {
 
-		public Point(int row, int col, int weight) {
-			super();
-			this.row = row;
-			this.col = col;
-			this.weight = weight;
-		}
+            N = Integer.parseInt(br.readLine());
+            if (N == 0)
+                break;
+            map = new int[N][N];
+            rupee=new int[N][N];
+            for (int i = 0; i < N; i++) {
 
-		@Override
-		public int compareTo(Point o) {
-			// weight오름차순
-			return this.weight - o.weight;
-		}
-	}
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-	public static int dijkstra() {
+            sb.append("Problem " + tc + ": " + dijkstra() + "\n");
+            tc++;
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 
-		pq = new PriorityQueue<>();
-		pq.offer(new Point(0, 0, map[0][0]));
+    public static int dijkstra() {
 
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(rupee[i], 1_000_000_000);
-		}
-		rupee[0][0] = map[0][0];
+        Queue<Point> pq = new PriorityQueue<>();
+        pq.offer(new Point(0, 0, map[0][0]));
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(rupee[i], INF);
+        }
+        rupee[0][0] = map[0][0];
 
-		while (!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
+            Point now = pq.poll();
+            if (now.row == N - 1 && now.col == N - 1) {
+                return now.w;
+            }
 
-			Point now = pq.poll();
+            for(int d=0;d<4;d++){
+                int nr=now.row+dr[d];
+                int nc=now.col+dc[d];
 
-			if (now.row == N - 1 && now.col == N - 1) {
-				return now.weight;
-			}
-			for (int d = 0; d < 4; d++) {
+                if(nr<0||nr>=N||nc<0||nc>=N)
+                    continue;
 
-				int nr = now.row + dr[d];
-				int nc = now.col + dc[d];
+                if(rupee[nr][nc]>now.w+map[nr][nc]){
+                    rupee[nr][nc]=now.w+map[nr][nc];
+                    pq.add(new Point(nr,nc,rupee[nr][nc]));
+                }
+            }
 
-				if (nr < 0 || nr >= N || nc < 0 || nc >= N)
-					continue;
+        }
+        return 0;
+    }
 
-				if (rupee[nr][nc] > now.weight + map[nr][nc]) {
+    public static class Point implements Comparable<Point> {
+        int row;
+        int col;
+        int w;
 
-					rupee[nr][nc] = now.weight + map[nr][nc];
-					pq.offer(new Point(nr, nc, rupee[nr][nc]));
-				}
-			}
-		}
-		return 0;
-	}
+        public Point(int row, int col, int w) {
+            this.row = row;
+            this.col = col;
+            this.w = w;
+        }
 
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int tc = 1;
-		while (true) {
-
-			N = Integer.parseInt(br.readLine());
-			if (N == 0)
-				break;
-
-			map = new int[N][N];
-			rupee = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			// 입력끝
-
-			// 다익스트라
-
-			sb.append("Problem " + tc + ": " + dijkstra() + "\n");
-			tc++;
-		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
+        @Override
+        public int compareTo(Point o) {
+            return this.w - o.w;
+        }
+    }
 }
