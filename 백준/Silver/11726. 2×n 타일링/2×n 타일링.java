6f3b7ecdce93eb80763feb78bc86a3eb1bdc6077
2pearl <74369702+2pearl@users.noreply.github.com>
2024-07-01 15:24:59 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,31 +1,27 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
+    static int N;
+    static int[] dp;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
 
-		int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-		dp[0] = 1;
-		dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10_007;
+        }
 
-		for (int i = 2; i <= n; i++) {
-
-			dp[i] = (dp[i - 1] + dp[i - 2]) % 10_007;
-		}
-		bw.write(String.valueOf(dp[n]));
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
+        bw.write(String.valueOf(dp[N]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
